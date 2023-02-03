2bd5a9730ddb48141a3857f5ffe2114cc9619b7d
J-P Nurmi <jp.nurmi@canonical.com>
2023-02-03 15:53:50 +0100

    Prettify result handling

--- lib/src/widgets/number_view.dart
+++ lib/src/widgets/number_view.dart
@@ -30,15 +30,32 @@ class NumberView extends StatelessWidget {
             duration: kThemeAnimationDuration,
             child: Align(
               alignment: Alignment.topCenter,
-              child: result == null
-                  ? const YaruCircularProgressIndicator()
-                  : result!.isError
-                      ? Text(result!.asError!.error.toString())
-                      : Text(result!.asValue!.value.text),
+              child: result.when(
+                loading: () => const YaruCircularProgressIndicator(),
+                success: (value) => Text(value.text),
+                error: (error) => Text(error.toString()),
+              ),
             ),
           ),
         ),
       ],
     );
+  }
+}
+
+extension on Result<Number>? {
+  T when<T>({
+    required T Function() loading,
+    required T Function(Number) success,
+    required T Function(Object) error,
+  }) {
+    final that = this;
+    if (that == null) {
+      return loading();
+    } else if (that.isError) {
+      return error(that.asError!.error);
+    } else {
+      return success(that.asValue!.value);
+    }
   }
 }
