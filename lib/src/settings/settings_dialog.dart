ac7b55fb37d12a6877b95e821aeb5c8f74469a9d
J-P Nurmi <jp.nurmi@canonical.com>
2023-02-10 22:52:49 +0100

    Pass theme preview builder to make the settings dialog reusable

--- lib/src/settings/settings_dialog.dart
+++ lib/src/settings/settings_dialog.dart
@@ -7,15 +7,22 @@ import 'package:yaru_widgets/yaru_widgets.dart';
 import 'settings.dart';
 import 'settings_widgets.dart';
 
-Future<void> showSettingsDialog({required BuildContext context}) {
+Future<void> showSettingsDialog({
+  required BuildContext context,
+  WidgetBuilder? themePreviewBuilder,
+}) {
   return showDialog(
     context: context,
-    builder: (context) => const SettingsDialog(),
+    builder: (context) => SettingsDialog(
+      themePreviewBuilder: themePreviewBuilder,
+    ),
   );
 }
 
 class SettingsDialog extends StatelessWidget {
-  const SettingsDialog({super.key});
+  const SettingsDialog({super.key, this.themePreviewBuilder});
+
+  final WidgetBuilder? themePreviewBuilder;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +42,7 @@ class SettingsDialog extends StatelessWidget {
                 SizedBox(
                   height: MediaQuery.of(context).size.width * 0.2,
                   child: ThemeSelector(
+                    previewBuilder: themePreviewBuilder,
                     theme: context.select((Settings s) => s.theme),
                     onChanged: context.read<Settings>().setTheme,
                   ),
